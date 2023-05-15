21dc45418def9735126a1abcfff5f68c2681e956
juliekrabek123 <hjq973@alumni.ku.dk>
2023-05-15 14:12:31 +0200

    yes yes

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from  matplotlib.colors import LinearSegmentedColormap
 
 def extract_data(barsel_true):
     
@@ -10,23 +11,23 @@ def extract_data(barsel_true):
     """
 
     # a. Extract varaibles for mother 
-    Mother_all = barsel_true[(barsel_true['FARUD'] == 'All fathers, regardless of education') 
-                              & (barsel_true['MORUD'] == 'All mothers, regardless of education') 
-                              & (barsel_true['TAL'] == 'Mother - days on parental leave (benefits) after birth on average') ]
+    Mother_all = barsel_true[(barsel_true['Dad_educ'] == 'All fathers, regardless of education') 
+                              & (barsel_true['Mom_educ'] == 'All mothers, regardless of education') 
+                              & (barsel_true['Numb'] == 'Mother - days on parental leave (benefits) after birth on average') ]
     # b. Cut data 
-    Mother_all = Mother_all[['TID', 'INDHOLD']]
+    Mother_all = Mother_all[['Year', 'Count']]
 
     # c. Extract varaibles for father 
-    Father_all = barsel_true[(barsel_true['MORUD'] == 'All mothers, regardless of education') 
-                              & (barsel_true['FARUD'] == 'All fathers, regardless of education') 
-                              & (barsel_true['TAL'] == 'Father - days on parental leave (benefits) after birth on average') ]
+    Father_all = barsel_true[(barsel_true['Mom_educ'] == 'All mothers, regardless of education') 
+                              & (barsel_true['Dad_educ'] == 'All fathers, regardless of education') 
+                              & (barsel_true['Numb'] == 'Father - days on parental leave (benefits) after birth on average') ]
     # d. Cut data 
-    Father_all = Father_all[['TID', 'INDHOLD']]
+    Father_all = Father_all[['Year', 'Count']]
     
     return Mother_all, Father_all
 
 
-def manipulate_data(df):
+def cut_data(df):
 
     """
     Input:   Dataframe 
@@ -35,35 +36,35 @@ def manipulate_data(df):
     """
 
     # a. Cut the data for the father such that the mother's education is constant
-    Father_educ = df[df['MORUD'] == 'All mothers, regardless of education']
+    Father_educ = df[df['Mom_educ'] == 'All mothers, regardless of education']
     
     # b. Remove variables
-    Father_educ = Father_educ[(Father_educ['TAL'] != 'Number of couples') 
-                           & (Father_educ['TAL'] != 'Mother - days on parental leave (benefits) after birth on average') 
-                           & (Father_educ['TAL'] != 'Mother - days on parental leave (benefits) before birth on average')
-                           & (Father_educ['FARUD'] != 'All fathers, regardless of education')]
+    Father_educ = Father_educ[(Father_educ['Numb'] != 'Number of couples') 
+                           & (Father_educ['Numb'] != 'Mother - days on parental leave (benefits) after birth on average') 
+                           & (Father_educ['Numb'] != 'Mother - days on parental leave (benefits) before birth on average')
+                           & (Father_educ['Dad_educ'] != 'All fathers, regardless of education')]
     
     # c. Cut data further
-    Father_educ = Father_educ[['FARUD', 'TID', 'INDHOLD']]
+    Father_educ = Father_educ[['Dad_educ', 'Year', 'Count']]
     
     # d. Rename the education variables
-    Father_educ['FARUD'] = Father_educ['FARUD'].str.replace('Father ', '').str.title()
+    Father_educ['Dad_educ'] = Father_educ['Dad_educ'].str.replace('Father ', '').str.title()
     
     # e. Cut the data for the mother such that the father's education is constant
-    Mother_educ = df[df['FARUD'] == 'All fathers, regardless of education']
+    Mother_educ = df[df['Dad_educ'] == 'All fathers, regardless of education']
     
     # f. Remove variables
-    Mother_educ = Mother_educ[(Mother_educ['TAL'] != 'Number of couples') 
-                           & (Mother_educ['TAL'] != 'Father - days on parental leave (benefits) after birth on average') 
-                           & (Mother_educ['TAL'] != 'Mother - days on parental leave (benefits) before birth on average') 
-                           & (Mother_educ['MORUD'] != 'All mothers, regardless of education')]
+    Mother_educ = Mother_educ[(Mother_educ['Numb'] != 'Number of couples') 
+                           & (Mother_educ['Numb'] != 'Father - days on parental leave (benefits) after birth on average') 
+                           & (Mother_educ['Numb'] != 'Mother - days on parental leave (benefits) before birth on average') 
+                           & (Mother_educ['Mom_educ'] != 'All mothers, regardless of education')]
     
     # g. Cut data further
-    Mother_educ = Mother_educ[['MORUD', 'TID', 'INDHOLD']]
+    Mother_educ = Mother_educ[['Mom_educ', 'Year', 'Count']]
     
     # h. Rename the education variables
-    Mother_educ['MORUD'] = Mother_educ['MORUD'].str.replace('Mother ', '').str.title()
-    Mother_educ['MORUD'] = Mother_educ['MORUD'].str.replace('Tertirary', 'Tertiary').str.title()
+    Mother_educ['Mom_educ'] = Mother_educ['Mom_educ'].str.replace('Mother ', '').str.title()
+    Mother_educ['Mom_educ'] = Mother_educ['Mom_educ'].str.replace('Tertirary', 'Tertiary').str.title()
     
     return Father_educ, Mother_educ
 
@@ -78,26 +79,26 @@ def plot_parental_leave(Father_educ, Mother_educ):
     fig = plt.figure()
 
     # b. Define ax to be the first of the two subplots
-    ax = fig.add_subplot(2,1,1)
+    ax = fig.add_subplot(2,1,2)
     # c. Extract the name of each education and plot for each for father's education
-    for label, group in Father_educ.groupby(['FARUD']):
-        group.plot(x='TID', y='INDHOLD', ax=ax, style='-', label=label)
+    for label, group in Father_educ.groupby(['Dad_educ']):
+        group.plot(x='Year', y='Count', ax=ax, style='-', label=label)
     # d. Add labels, title and legend
-    ax.set_xlabel('')
-    ax.set_xticklabels([])
+    ax.set_xlabel('Year')
     ax.set_ylabel('Days')
-    ax.set_title('The number of days of parental leave for father')
+    ax.set_title('The number of days of parental leave for mother')
     ax.legend().remove()
 
     # e. Define ax to be the second of the two subplots
-    ax1 = fig.add_subplot(2,1,2)
+    ax1 = fig.add_subplot(2,1,1)
     # f. Extract the name of each education and plot for each for mother's education
-    for label, group in Mother_educ.groupby(['MORUD']):
-        group.plot(x='TID', y='INDHOLD', ax=ax1, style='-', label=label)
+    for label, group in Mother_educ.groupby(['Mom_educ']):
+        group.plot(x='Year', y='Count', ax=ax1, style='-', label=label)
     # g. Add labels, title and legend
-    ax1.set_xlabel('Year')
+    ax1.set_xlabel('')
+    ax1.set_xticklabels([])
     ax1.set_ylabel('Days')
-    ax1.set_title('The number of days of parental leave for mother')
+    ax1.set_title('The number of days of parental leave for father')
     ax1.legend().remove()
 
     # h. Create a single legend for both subplots
@@ -119,54 +120,62 @@ def calculate_father_share(barsel_true, year):
     """
 
     # a. Cut data to contain education for mother and father 
-    Father_part_educ_1 = barsel_true[(barsel_true['FARUD'] != 'All fathers, regardless of education') 
-                               & (barsel_true['MORUD'] != 'All mothers, regardless of education') 
-                               & (barsel_true['TAL'] != 'Number of couples')
-                               & (barsel_true['TAL'] != 'Mother - days on parental leave (benefits) before birth on average')
-                               & (barsel_true['TID'] == year)]
+    Father_part_educ_1 = barsel_true[(barsel_true['Dad_educ'] != 'All fathers, regardless of education') 
+                               & (barsel_true['Mom_educ'] != 'All mothers, regardless of education') 
+                               & (barsel_true['Numb'] != 'Number of couples')
+                               & (barsel_true['Numb'] != 'Mother - days on parental leave (benefits) before birth on average')
+                               & (barsel_true['Year'] == year)]
     
     # b. Cut data 
-    Father_part_educ_1 = Father_part_educ_1[['TAL', 'MORUD', 'FARUD', 'INDHOLD']]
+    Father_part_educ_1 = Father_part_educ_1[['Numb', 'Mom_educ', 'Dad_educ', 'Count']]
     
     # c. Rename the TAL column
-    Father_part_educ_1['TAL'] = Father_part_educ_1['TAL'].str.split(' ').str[0]
+    Father_part_educ_1['Numb'] = Father_part_educ_1['Numb'].str.split(' ').str[0]
     
     # d. Rename MORUD and FARUD
-    Father_part_educ_1['MORUD'] = Father_part_educ_1['MORUD'].str.replace('Mother ', '').str.title()
-    Father_part_educ_1['MORUD'] = Father_part_educ_1['MORUD'].str.replace('Tertirary', 'Tertiary').str.title()
-    Father_part_educ_1['FARUD'] = Father_part_educ_1['FARUD'].str.replace('Father ', '').str.title()
+    Father_part_educ_1['Mom_educ'] = Father_part_educ_1['Mom_educ'].str.replace('Mother ', '').str.title()
+    Father_part_educ_1['Mom_educ'] = Father_part_educ_1['Mom_educ'].str.replace('Tertirary', 'Tertiary').str.title()
+    Father_part_educ_1['Dad_educ'] = Father_part_educ_1['Dad_educ'].str.replace('Father ', '').str.title()
     
     # e. Split dataframe into one for mother and father 
-    Father_part_educ_1_m = Father_part_educ_1[Father_part_educ_1['TAL'] == 'Mother']
-    Father_part_educ_1_f = Father_part_educ_1[Father_part_educ_1['TAL'] == 'Father']
+    Father_part_educ_1_m = Father_part_educ_1[Father_part_educ_1['Numb'] == 'Mother']
+    Father_part_educ_1_f = Father_part_educ_1[Father_part_educ_1['Numb'] == 'Father']
     
     # f. Merge the two dataframes 
-    Father_part_educ_1 = pd.merge(Father_part_educ_1_m, Father_part_educ_1_f, on = ['MORUD', 'FARUD'], how = 'left')
+    Father_part_educ_1 = pd.merge(Father_part_educ_1_m, Father_part_educ_1_f, on = ['Mom_educ', 'Dad_educ'], how = 'left')
     
     # g. Cut data 
-    Father_part_educ_1 = Father_part_educ_1[['MORUD', 'FARUD', 'INDHOLD_x', 'INDHOLD_y']]
+    Father_part_educ_1 = Father_part_educ_1[['Mom_educ', 'Dad_educ', 'Count_x', 'Count_y']]
     
     # h. Rename the columns 
-    Father_part_educ_1.columns = ['MORUD', 'FARUD', 'mother', 'father']
+    Father_part_educ_1.columns = ['Mom_educ', 'Dad_educ', 'mother', 'father']
     
     # i. Find fathers share of parential leave 
     Father_part_educ_1['FS'] = Father_part_educ_1['father']/(Father_part_educ_1['father'] + Father_part_educ_1['mother'])*100
     Father_part_educ_1['FS'] = Father_part_educ_1['FS'].round(1)
     
     # j. Make pivot for fathers share in % 
-    Father_part_educ_2020_pivot_procent = Father_part_educ_1.pivot_table(index='MORUD', columns='FARUD', values='FS').round(1)
+    Father_part_educ_2020_pivot_procent = Father_part_educ_1.pivot_table(index='Mom_educ', columns='Dad_educ', values='FS').round(1)
     Father_part_educ_2020_pivot_procent = Father_part_educ_2020_pivot_procent.reindex(['Lower Secondary', 'Upper Secondary', 'Short Cycle Tertiary', 'Bachelor', 'Master'], axis=1)
     Father_part_educ_2020_pivot_procent = Father_part_educ_2020_pivot_procent.reindex(['Lower Secondary', 'Upper Secondary', 'Short Cycle Tertiary', 'Bachelor', 'Master'])
     
     # k. Make pivot for fathers share in days 
-    Father_part_educ_2020_pivot_days = Father_part_educ_1.pivot_table(index='MORUD', columns='FARUD', values='father').round(1)
+    Father_part_educ_2020_pivot_days = Father_part_educ_1.pivot_table(index='Mom_educ', columns='Dad_educ', values='father').round(1)
     Father_part_educ_2020_pivot_days = Father_part_educ_2020_pivot_days.reindex(['Lower Secondary', 'Upper Secondary', 'Short Cycle Tertiary', 'Bachelor', 'Master'], axis=1)
     Father_part_educ_2020_pivot_days = Father_part_educ_2020_pivot_days.reindex(['Lower Secondary', 'Upper Secondary', 'Short Cycle Tertiary', 'Bachelor', 'Master'])
     
     # l. color the pivots 
+    # i. set colors
+    cmap = LinearSegmentedColormap.from_list('rg',["r", "yellow", "g"], N=256) 
+    # ii. color the procent pivot 
     Father_part_educ_2020_pivot_procent_color = Father_part_educ_2020_pivot_procent.style.background_gradient(
-        cmap = 'YlGn', axis = None)
+        cmap = cmap, axis = None)
+    # ii. color the days pivot 
     Father_part_educ_2020_pivot_days_color = Father_part_educ_2020_pivot_days.style.background_gradient(
-        cmap = 'YlGn', axis = None)
+        cmap = cmap, axis = None)
+    
+    # m. set precision for the pivot tables
+    Father_part_educ_2020_pivot_procent_color = Father_part_educ_2020_pivot_procent_color.format(precision=1)
+    Father_part_educ_2020_pivot_days_color = Father_part_educ_2020_pivot_days_color.format(precision=1)
 
-    return  Father_part_educ_2020_pivot_procent_color, Father_part_educ_2020_pivot_days_color
+    return Father_part_educ_2020_pivot_procent_color, Father_part_educ_2020_pivot_days_color
