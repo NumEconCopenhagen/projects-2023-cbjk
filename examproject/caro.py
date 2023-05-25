50bc7a0f30d463686f0ddfdbcc896ff1f0a03f1e
CarolineBergholdtH <fgr164@alumni.ku.dk>
2023-05-25 21:26:41 +0200

    yes

--- examproject/caro.py
+++ examproject/caro.py
@@ -1,15 +1,13 @@
-from random import seed
 import numpy as np
-from scipy import optimize
-import matplotlib.pyplot as plt
 from types import SimpleNamespace
 
 class labor:
     
     def __init__(self,**kwargs): # called when created
-
+        """
+        Initialize the labor class.
+        """
         par = self.par = SimpleNamespace()
-        sol = self.sol = SimpleNamespace()
         
         # a. baseline parameters
         par.eta = 0.5
@@ -35,43 +33,57 @@ class labor:
          # note: "kwargs" is a dictionary with keyword arguments
     
     def H(self, out=1, Delta=0.05):
-        
+        """
+        Calculate the value of H based on given settings.
+        out=1 is the original policy
+        out=2 is the modified policy
+        out=3 is our own policy
+        """
+        # a. set starting values and par
         par = self.par
         H0 = 0
 
-
+        # b. iterate over the random shock series
         for i in range(self.K):
             kappa = np.empty(0)
             L = np.empty(0)
             log_kappa_t0 = np.log(self.kappa_0)
             np.random.seed(i)
             epsilon = np.random.normal(-0.5*par.sigma_e**2, par.sigma_e, len(par.t))
+            # i. calculate kappa for each epsilon
             for e in epsilon:
                 kap0 = np.exp(log_kappa_t0)
                 kappa = np.append(kappa,kap0)
                 log_kappa_t = par.rho*log_kappa_t0 + e
                 log_kappa_t0 = log_kappa_t.copy()
             
-            l_0 = self.l_0 
-            if out == 1:
+            l_0 = self.l_0  # make sure that the starting value is the same for each iteration
+            
+            # ii. calculate l for a given policy
+
+            if out == 1: #original policy
 
                 for kap in kappa:
                     L = np.append(L,l_0)
                     l_0 = self.l_star(kap)
             
-            if out == 2:
+            if out == 2: #modified policy
                 for kap in kappa:
                     L = np.append(L,l_0)
                     l_star = self.l_star(kap)
-                    if abs(l_0-l_star) > Delta:
+                    # o. change l_star if difference larger than Delta
+                    if abs(l_0-l_star) > Delta: 
                         l_0 = l_star
-            if out == 3:
+            if out == 3: #our policy
                 for t, kap in enumerate(kappa):
                     L = np.append(L,l_0)
                     l_star = self.l_star(kap)
-                    if abs(epsilon[t]) > par.sigma_e:
+                    # oo. change l_star if epsilon larger than the variance
+                    if abs(epsilon[t]) < par.sigma_e:
                         l_0 = l_star
-            h = 0
+
+            # iii. calculate h given l and kappa            
+            h = 0 # make sure that the starting value is the same for each iteration
             for t in range(120):
                 if t==0:
                     L[t-1] = self.l_0
@@ -81,12 +93,19 @@ class labor:
                 else:
                     h += par.R**(-t)*(kappa[t]*(L[t]**(1-par.eta))-par.w*L[t]-par.iota)
             H0 += h
-                   
-        H = (H0/self.K)
+
+        # iiii. find H by taken mean of each iteration           
+        H = (H0/self.K) #
         return H
 
 
     def l_star(self, kappa):
+        """
+        Calculate the value of l_star based on given kappa.
+        """
+        # a. set par
         par = self.par
+
+        # b. calculate l 
         l_star = (((1-par.eta)*kappa)/par.w)**(1/par.eta)
         return l_star
